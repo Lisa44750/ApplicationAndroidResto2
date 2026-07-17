71791666469b3e6e056929412de65425dbc8b463
Lisa44750 <153626463+Lisa44750@users.noreply.github.com>
2026-07-17 20:43:48 +0200

    Add files via upload

---
+++ getAllRestoJSON.php
@@ -0,0 +1,29 @@
+<?php
+try {
+// Connexion à la base de données MySql
+$db="resto2";
+$dbhost="localhost";
+$dbport=3306;
+$dbuser="root";
+$dbpasswd="joliverie";
+ 
+$connexion = new PDO('mysql:host='.$dbhost.';port='.$dbport.';dbname='.$db.'', $dbuser, $dbpasswd);
+$connexion->exec("SET CHARACTER SET utf8");
+                        
+        $reponse=$connexion->prepare("SELECT nomR, villeR FROM resto;");
+        $reponse->execute();
+        $datas = array();
+         
+        while($res=$reponse->fetch(PDO::FETCH_ASSOC)) {
+            $datas['restos'][]=$res;
+			
+        }
+             
+      echo json_encode($datas);
+	   
+}
+catch (Exception $e) {
+die('Erreur : ' . $e->getMessage());
+}
+
+?>
